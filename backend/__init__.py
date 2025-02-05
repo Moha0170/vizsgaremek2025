880fac19f12bc1b399ab240fa698a3c96c935579
Diák <martonsoos10@gmail.com>
2025-02-05 09:39:59 +0100

    Backend cors fix

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins=["http://localhost:5173"])
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/hypercharge?charset=utf8mb4'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
