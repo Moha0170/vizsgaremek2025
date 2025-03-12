bc1942f2ab22040dd7ca8081c5cd8f9fda754223
Moha0170 <mohalaja06@gmail.com>
2025-03-12 14:27:02 +0100

    flasgger setup, req txt

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_argon2 import Argon2
+from flasgger import Swagger
 
 db = SQLAlchemy()
 app = Flask(__name__)
