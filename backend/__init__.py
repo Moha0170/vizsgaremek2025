1bb349b9e960045c38ae6eef577701074c82c407
Moha0170 <mohalaja06@gmail.com>
2025-03-12 09:12:11 +0100

    errorork kezelese, adatbazis frissites, nem hasznalt feature kitorlese

--- backend/__init__.py
+++ backend/__init__.py
@@ -19,12 +19,10 @@ def create_app():
     from .routes.admin import admin_bp
     from .routes.cart import cart_bp
     from .routes.market import market_bp
-    from .routes.gourmetgo import gourmetgo_bp
     from .routes.profile import profile_bp
 
     app.register_blueprint(admin_bp)
     app.register_blueprint(cart_bp)
-    app.register_blueprint(gourmetgo_bp)
     app.register_blueprint(market_bp)
     app.register_blueprint(profile_bp)
 
