fc79ece7899aeb8e102091c00fe390defce83d44
Diák <martonsoos10@gmail.com>
2025-03-19 14:28:10 +0100

    rendelés stílus

--- frontend/src/components/Transaction.tsx
+++ frontend/src/components/Transaction.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
-import "../style/orders.css";
+import "../style/transaction.css";
 
 
 interface CartItem {
