a4be5150c794a910ffd677eea5262acdf87c767f
Moha0170 <mohalaja06@gmail.com>
2025-03-20 12:11:15 +0100

    market.py dokumentacio

--- backend/routes/market.py
+++ backend/routes/market.py
@@ -1,26 +1,27 @@
 from flask import Blueprint, jsonify, request
 from sqlalchemy import text
+from flasgger import swag_from
 from backend import db
-from backend.models.model import Termek
 
 market_bp = Blueprint("market_bp", __name__, url_prefix="/market")
 
-#ezt még fixálni kell
 @market_bp.route("/allProducts", methods=['GET'])
+@swag_from("../docs/market_allProducts.yaml")
 def allProducts():
-    results = Termek.query.all()
-    data = [{column.name: getattr(row, column.name) for column in Termek.__table__.columns} for row in results]
-    return jsonify(data)
+    results = db.session.execute(text("SELECT * FROM `termekek`")).fetchall()
+    return jsonify([row._asdict() for row in results])
 
 @market_bp.route("/getProductByKat/<kat>", methods=['GET'])
+@swag_from("../docs/market_getProductById.yaml")
 def getProducts(kat):
     results = db.session.execute(text("SELECT * FROM `termekek` WHERE `kat` = :kat"), {'kat': kat})
     return jsonify([row._asdict() for row in results])
 
 @market_bp.route("/getProduct/<id>", methods=['GET'])
+@swag_from("../docs/market_getProduct.yaml")
 def getProductById(id):
     results = db.session.execute(text("SELECT * FROM `termekek` WHERE `id` = :id"), {'id': id}).fetchone()
-    results = jsonify([row._asdict() for row in results])
+    results = jsonify(results._asdict())
     if not results:
         return jsonify({"error": "Termék nem található"}), 404
     else:
